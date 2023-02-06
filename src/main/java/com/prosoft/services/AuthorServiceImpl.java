88cdbbe724ba7df9cb55a5ad035927a7dc339fd1
sproshchaev <sproshchaev@gmail.com>
2023-02-06 09:31:31 +0500

    Added @Transactional

--- src/main/java/com/prosoft/services/AuthorServiceImpl.java
+++ src/main/java/com/prosoft/services/AuthorServiceImpl.java
@@ -4,6 +4,7 @@ import com.prosoft.entities.Author;
 import com.prosoft.repositories.AuthorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,26 +20,31 @@ public class AuthorServiceImpl implements AuthorService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Author> getAllAuthors() {
         return authorRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Optional<Author> getAuthorById(long id) {
         return authorRepository.findById(id);
     }
 
     @Override
+    @Transactional
     public void updateAuthor(Author author) {
         authorRepository.save(author);
     }
 
     @Override
+    @Transactional
     public void deleteAuthor(Author author) {
         authorRepository.delete(author);
     }
 
     @Override
+    @Transactional
     public Author createAuthor(Author author) {
         return authorRepository.save(author);
     }
